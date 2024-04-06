5f5419d184bf2d3f68e90949b10648a72b96ed55
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-04-07 05:41:15 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ $(document).ready(function () {
 });
 const ctx = document.getElementById('myChart');
 
+$(".datax .questionx p, .datax .questionx li, .datax .questionx h3, .datax .questionx small, .datax .questionx canvas").attr({ "data-wow-delay": "0.5s" }).addClass('wow animate__animated animate__fadeIn')
+$(".datax .questionx img").attr({ "data-wow-delay": "0.5s" }).addClass('wow animate__animated animate__zoomIn')
+
 
 
 new Chart(ctx, {
